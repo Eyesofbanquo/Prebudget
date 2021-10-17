0dc334300146054cda2b8584416940d3195cae70
Markim Shaw <ms79723@gmail.com>
2021-10-17 00:13:40 -0500

    Git ignore

--- Prebudget/PrebudgetApp.swift
+++ Prebudget/PrebudgetApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct PrebudgetApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
