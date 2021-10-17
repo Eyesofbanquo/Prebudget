d69bbda00fd74e7ec88e3f58567a913fd9ea581a
Markim Shaw <ms79723@gmail.com>
2021-10-16 22:55:10 -0500

    Initial Commit

---
+++ Prebudget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prebudget
+//
+//  Created by Markim Shaw on 10/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prebudget/PrebudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrebudgetApp.swift
+//  Prebudget
+//
+//  Created by Markim Shaw on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct PrebudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
